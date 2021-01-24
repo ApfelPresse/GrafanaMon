199514e76c1603a13c88fc77f3353add19920aea
Kevin <kevin.stiefel@deutschebahn.com>
2021-01-24 16:26:08 +0100

    update

--- api2/boerse_frankfurt_rest/app/interface.py
+++ api2/boerse_frankfurt_rest/app/interface.py
@@ -1,8 +1,47 @@
+from flasgger import Swagger
 from flask import Blueprint
+from flask import jsonify
+import flask
 
 app = Blueprint('interface', __name__)
 
+@app.route('/colors/<palette>/')
+def colors(palette):
+    """Example endpoint returning a list of colors by palette
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: palette
+        in: path
+        type: string
+        enum: ['all', 'rgb', 'cmyk']
+        required: true
+        default: all
+    definitions:
+      Palette:
+        type: object
+        properties:
+          palette_name:
+            type: array
+            items:
+              $ref: '#/definitions/Color'
+      Color:
+        type: string
+    responses:
+      200:
+        description: A list of colors (may be filtered by palette)
+        schema:
+          $ref: '#/definitions/Palette'
+        examples:
+          rgb: ['red', 'green', 'blue']
+    """
+    all_colors = {
+        'cmyk': ['cian', 'magenta', 'yellow', 'black'],
+        'rgb': ['red', 'green', 'blue']
+    }
+    if palette == 'all':
+        result = all_colors
+    else:
+        result = {palette: all_colors.get(palette)}
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask"
+    return jsonify(result)
--- api2/boerse_frankfurt_rest/app/main.py
+++ api2/boerse_frankfurt_rest/app/main.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from flask_graphite import FlaskGraphite
-
 import health_check
 import interface
+from flasgger import Swagger
+from flask import Flask
+from flask_graphite import FlaskGraphite
 from utils import get_env
 
 app = Flask(__name__)
+swagger = Swagger(app)
+
 app.config["FLASK_GRAPHITE_HOST"] = get_env("GRAPHITE_SERVER", "127.0.0.1")
 app.config["FLASK_GRAPHITE_PORT"] = 2003
 
@@ -15,4 +17,4 @@ app.register_blueprint(interface.app)
 app.register_blueprint(health_check.app)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0')
